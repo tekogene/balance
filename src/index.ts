6c62e00670d4b75a1cb63f997500c437d37ff0a2
Eugene S <eugeniu.sincovchi@tekoway.com>
2022-02-21 09:48:21 +0200

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,87 +1,129 @@
 class StringBuilder {
-  /**
-   * Initial balance
-   */
-  private bNum: number = 0;
+  private balance: number = 0;
+  private rex = {
+    matchDigitsAsFloat(str: string = ""): RegExpMatchArray | null {
+      return str.match(/\d+(?:\.\d+)?/);
+    },
+    matchRowData(
+      str: string = ""
+    ): { id: string; text: string; amount: string } | null {
+      const match = str.match(
+        /(?<id>\d+)(?<text>.+?)(?<amount>\d+(?:\.\d+)?)/i
+      );
 
-  constructor(str: string = "") {
-    const splinters = str.split(/\n/);
+      if (!match) {
+        return null;
+      }
 
-    // get first
-    const first = splinters.shift() || "";
+      return {
+        id: match.groups!["id"],
+        text: match.groups!["text"],
+        amount: match.groups!["amount"],
+      };
+    },
+  };
+  private substract = (first: number, second: number): number => {
+    return Number((first - second).toFixed(2));
+  };
 
-    // match an integer for balance
-    const match = first.match(/\d+(?:\.\d+)?/);
+  constructor(book: string = "") {
+    const bookRows = book.split(/\n/);
+    const firstRow = bookRows.shift() || "";
+
+    const match = this.rex.matchDigitsAsFloat(firstRow);
     if (match) {
-      this.bNum = Number(match[0]);
+      this.balance = Number(match[0]);
     }
 
-    // parse splinters
-    splinters.forEach((splint) => {
-      const match = splint.match(/(\d+)(.+?)(\d+(?:\.\d+)?)/i);
-      if (match) {
-        // add new row
-        this.rows.push([
-          // id
-          Number(match[1]),
-          // clean string
-          match[2].replace(/[^\w|\s]/g, "").trim(),
-          // money
-          Number(match[3]),
-        ]);
-      }
+    bookRows.forEach((row) => {
+      this.setRowElements(row);
     });
   }
 
-  /**
-   * Parsed rows
-   */
-  private rows: [number, string, number][] = [];
+  private cleanString(str: string): string {
+    return str.replace(/[^\w|\s]/g, "").trim();
+  }
 
-  /**
-   * Final build
-   * @returns array
-   */
-  public getFinal(): string {
-    const rows = this.rows.reduce(
-      (rows: [string[], number], el: [number, string, number]) => {
-        rows[1] -= el[2];
-        // build row string
-        const r =
-          String(el[0]).padStart(3, "0") +
-          " " +
-          el[1] +
-          " " +
-          el[2].toFixed(2) +
-          " " +
-          "Balance" +
-          " " +
-          rows[1].toFixed(2);
-        rows[0].push(r);
-        return rows;
+  private getOriginAsString(): string {
+    return `Original Balance: ${this.balance.toFixed(2)}`;
+  }
+
+  private getTotalAsString(total: number): string {
+    return `Total expense  ${total.toFixed(2)}`;
+  }
+
+  private getAverageAsString(average: number): string {
+    return `Average expense  ${average.toFixed(2)}`;
+  }
+
+  private getRowAsString({
+    id,
+    text,
+    amount,
+    remainder,
+  }: {
+    id: number;
+    text: string;
+    amount: number;
+    remainder: number;
+  }): string {
+    return [
+      String(id).padStart(3, "0"),
+      text,
+      amount.toFixed(2),
+      "Balance",
+      remainder.toFixed(2),
+    ].join(" ");
+  }
+
+  private rowsElements: [number, string, number][] = [];
+
+  private setRowElements(str: string): void {
+    const rowData = this.rex.matchRowData(str);
+    if (!rowData) {
+      return;
+    }
+    const { id, text, amount } = rowData;
+    this.rowsElements.push([
+      Number(id),
+      this.cleanString(text),
+      Number(amount),
+    ]);
+  }
+
+  private getRowsAndRemainder(): [string[], number] {
+    return this.rowsElements.reduce(
+      ([finalRows, remainder]: [string[], number], [id, text, amount]) => {
+        remainder = this.substract(remainder, amount);
+        finalRows.push(
+          this.getRowAsString({
+            id,
+            text,
+            amount,
+            remainder,
+          })
+        );
+        return [finalRows, remainder];
       },
-      [[], this.bNum]
+      [[], this.balance]
     );
+  }
 
-    const total = this.bNum - rows[1];
+  public getFinalString(): string {
+    const [finalRows, remainder] = this.getRowsAndRemainder();
+    const totalExpense = this.substract(this.balance, remainder);
 
-    const text =
-      // build original balance string
-      `Original Balance: ${this.bNum.toFixed(2)}\r\n` +
-      // berge the rows
-      rows[0].join("\r\n") +
-      "\r\n" +
-      // build total expense string
-      `Total expense  ${total.toFixed(2)}\r\n` +
-      // build average expense string
-      `Average expense  ${(total / this.rows.length).toFixed(2)}`;
-
-    return text;
+    return [
+      this.getOriginAsString(),
+      ...finalRows,
+      this.getTotalAsString(totalExpense),
+      this.getAverageAsString(totalExpense / this.rowsElements.length),
+    ].join("\r\n");
   }
 }
 
 export function balance(book: string): string {
   const strBuilder = new StringBuilder(book);
 
-  return strBuilder.getFinal();
+  return strBuilder.getFinalString();
 }
